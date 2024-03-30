48d0ca3329b220d6984e57ec6ecd2bc800fc4cb3
eldanaomushova <eldana956@gmail.com>
2024-03-31 01:07:49 +0600

    bluetooth detected

--- lib/pages/connection_page.dart
+++ lib/pages/connection_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/styles/colors.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'dart:io' show Platform;
 
-class ConnectionPage extends StatelessWidget {
+class ConnectionPage extends StatefulWidget {
   const ConnectionPage({Key? key}) : super(key: key);
+
+  @override
+  _ConnectionPageState createState() => _ConnectionPageState();
+}
+
+class _ConnectionPageState extends State<ConnectionPage> {
+  bool _isScanning = false; // Initialize to false
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +33,7 @@ class ConnectionPage extends StatelessWidget {
             ),
             const Padding(
               padding: EdgeInsets.fromLTRB(0.0, 50, 40, 30),
-              child: BackButton( 
-                color: Colors.black, 
-              ),
+              child: BackButton(color: Colors.black),
             ),
             Align(
               alignment: Alignment.center,
@@ -34,46 +41,63 @@ class ConnectionPage extends StatelessWidget {
                 padding: const EdgeInsets.all(14.0),
                 child: Text(
                   "Please turn on the Bluetooth and do connection",
-                  textAlign: TextAlign.center, 
+                  textAlign: TextAlign.center,
                   style: AppStyles.mainText.copyWith(
                     fontSize: 20,
-                    fontWeight: FontWeight.bold, 
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
             ),
             Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 52), 
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (_) => ConnectionPage()));
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 40, 40, 40),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    side: const BorderSide(
-                      color: Colors.white, 
-                      width: 1, 
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 52),
+                child: Tooltip(
+                  message: _isScanning ? 'Stop scanning' : 'Start scanning', 
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      print("scanning");
+                      if (_isScanning) {
+                        await FlutterBluePlus.stopScan();
+                      } else {
+                        await FlutterBluePlus.startScan();
+                      }
+                      setState(() {
+                        _isScanning = !_isScanning; // Toggle the value of _isScanning
+                      });
+                    },
+
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 40, 40, 40),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        side: const BorderSide(
+                          color: Colors.white,
+                          width: 1,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.fromLTRB(60, 12, 60, 12),
-                  child: Text(
-                    'Connect to HC-05',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                    child: StreamBuilder<bool>(
+                      stream: FlutterBluePlus.isScanning,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        }
+
+                        if (snapshot.hasData) {
+                          final isScanning = snapshot.data!;
+                          _isScanning = isScanning;
+                          return Icon(isScanning ? Icons.stop : Icons.play_arrow);
+                        }
+
+                        return CircularProgressIndicator(); 
+                      },
                     ),
                   ),
                 ),
               ),
             ),
-          ),
           ],
         ),
       ),
